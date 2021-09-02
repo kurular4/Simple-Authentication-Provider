6561a8be5a7755346cafe8918b02691c1662277f
kurular4 <kurular4@gmail.com>
2021-09-02 19:27:50 +0300

    -json to pojo maven plugin added -ConditionalOnResource annotation added to depend on model schema -model generator module added

--- auto-configuration/src/main/java/com/kurular/simpleauthenticationprovider/autoconfiguration/SimpleAuthenticationProviderAutoConfiguration.java
+++ auto-configuration/src/main/java/com/kurular/simpleauthenticationprovider/autoconfiguration/SimpleAuthenticationProviderAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.kurular.simpleauthenticationprovider.autoconfiguration;
 
 import com.kurular.simpleauthenticationprovider.autoconfiguration.properties.SimpleAuthenticationProviderProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 @ConditionalOnWebApplication
+@ConditionalOnResource(resources = "/model-schema.json")
 @EnableConfigurationProperties(SimpleAuthenticationProviderProperties.class)
 public class SimpleAuthenticationProviderAutoConfiguration {
     private final SimpleAuthenticationProviderProperties properties;
