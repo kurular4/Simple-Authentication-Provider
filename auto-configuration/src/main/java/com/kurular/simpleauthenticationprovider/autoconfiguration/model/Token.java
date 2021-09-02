c6f5d44e71030b45c7153755a8a9b9e2900259d6
kurular4 <kurular4@gmail.com>
2021-09-02 18:32:03 +0300

    Duration default value added

--- auto-configuration/src/main/java/com/kurular/simpleauthenticationprovider/autoconfiguration/model/Token.java
+++ auto-configuration/src/main/java/com/kurular/simpleauthenticationprovider/autoconfiguration/model/Token.java
@@ -3,14 +3,19 @@ package com.kurular.simpleauthenticationprovider.autoconfiguration.model;
 import java.time.Duration;
 
 public class Token {
+    /**
+     * secret key to sign token
+     */
     private String secretKey;
 
     /**
+     * duration of token
+     * samples:
      * PT1.5S       = 1.5 Seconds
      * PT60S        = 60 Seconds
      * PT3M         = 3 Minutes
      * PT2H         = 2 Hours
      * P3DT5H40M30S = 3Days, 5Hours, 40 Minutes and 30 Seconds
      */
-    private Duration duration;
+    private Duration duration = Duration.ofDays(7);
 }
