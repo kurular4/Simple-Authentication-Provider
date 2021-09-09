2d5dca6778beed5fd8db55f2694c2ac06bbf4fe3
kurular4 <kurular4@gmail.com>
2021-09-09 19:21:41 +0300

    PasswordResetToken model added

---
+++ auto-configuration/src/main/java/com/kurular/simpleauthenticationprovider/autoconfiguration/model/auth/PasswordResetToken.java
@@ -0,0 +1,27 @@
+package com.kurular.simpleauthenticationprovider.autoconfiguration.model.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+public class PasswordResetToken {
+    private static final int EXPIRATION = 60 * 24;
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private Date expiryDate;
+}
